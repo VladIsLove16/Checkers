a9b01d74a315a6f71470920281f2d3afaaf64970
Ramilsibigatulin <sibigatulinramil@gmail.com>
2024-03-25 04:49:11 +0300

    создал проект ASP.NET core MVC. разметка нач.рег и осн.разделов

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IActionResult Index()
+        {
+            return View("start_reg/Index");
+        }
+        public IActionResult Reg_arbitr()
+        {
+            return View("start_reg/Reg_arbitr");
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication2/Controllers/StartRegController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+   
+        public class StartRegController : Controller
+        {
+            public IActionResult Index()
+            {
+                return View();
+            }
+        public IActionResult Entrance()
+        {
+            return View("~/Views/Home/start_reg/entrance.cshtml");
+        }
+
+        public IActionResult Reg_arbitr()
+        {
+            return View("~/Views/Home/start_reg/Reg_arbitr.cshtml");
+        }
+
+        public IActionResult Reg_player()
+        {
+            return View("~/Views/Home/start_reg/Reg_player.cshtml");
+        }
+    }
+   
+}
