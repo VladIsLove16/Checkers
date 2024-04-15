2aca16883123db6a5cd08d87694d74650fb75d70
EVOALI1 <91939916+EVOALI1@users.noreply.github.com>
2024-04-15 21:36:31 +0300

    Update OlympicSortSystem.cs

--- WebApplication2/Models/SortSystems/OlympicSortSystem.cs
+++ WebApplication2/Models/SortSystems/OlympicSortSystem.cs
@@ -1,66 +1,72 @@
-﻿//namespace WebApplication2.Models.SortSystems
-//{
-//    public class OlympicSortSystem
-//    {
-//        public List<RoundGames> CreateTournamentSchedule()
-//        {
-//            var tournamentSchedule = new List<RoundGames>();
-//            var participantOrder = RandomizeParticipantOrder();
-//            for (int i = 0; i < participantOrder.Count - 1; i++)
-//            {
-//                var games = new RoundGames();
-//                for (int pairIndex = 0; pairIndex < participantOrder.Count / 2; pairIndex++)
-//                {
-//                    var player1Order = participantOrder[pairIndex];
-//                    var player2Order = participantOrder[participantOrder.Count - 1 - pairIndex];
-//                    if (player1Order == 0 || player2Order == 0) continue;
+using System;
+using System.Collections.Generic;
 
-//                    var player1 = participantOrder[player1Order - 1];
-//                    var player2 = participantOrder[player2Order - 1];
+namespace WebApplication2.Models.SortSystems
+{
+    public class OlympicSortSystem
+    {
+        public List<RoundGames> CreateTournamentSchedule(int numberOfParticipants)
+        {
+            // Ensure the number of participants is valid
+            if (numberOfParticipants <= 0)
+                throw new ArgumentException("Number of participants must be greater than zero.");
 
-//                    if (tournamentSchedule.Count > 0 && tournamentSchedule[tournamentSchedule.Count - 1].Find(game => game.PlayerWhite == player1) != null)
-//                    {
-//                        games.Add(player2, player1);
-//                    }
-//                    else
-//                    {
-//                        games.Add(player1, player2);
-//                    }
-//                }
-//                tournamentSchedule.Add(games);
+            var tournamentSchedule = new List<RoundGames>();
+            var participantOrder = RandomizeParticipantOrder(numberOfParticipants);
 
-//                int elm = participantOrder[participantOrder.Count - 2];
-//                participantOrder.RemoveAt(participantOrder.Count - 2);
-//                participantOrder.Insert(0, elm);
-//            }
-//            return tournamentSchedule;
-//        }
-//        private List<int> RandomizeParticipantOrder()
-//        {
-//            var rnd = new Random();
-//            var startingElement = 1;
-//            var orderLength = participants.Length;
-//            if (participants.Length % 2 > 0)
-//            {
-//                startingElement--;
-//                orderLength++;
-//            }
-//            var participantOrder = new List<int>();
-//            for (int i = startingElement; i < startingElement + orderLength; i++)
-//            {
-//                participantOrder.Add(i);
-//            }
-//            for (int i = 0; i < participantOrder.Count - 1; i += 1)
-//            {
-//                int swapIndex = rnd.Next(i, participantOrder.Count);
-//                if (swapIndex != i)
-//                {
-//                    int temp = participantOrder[i];
-//                    participantOrder[i] = participantOrder[swapIndex];
-//                    participantOrder[swapIndex] = temp;
-//                }
-//            }
-//            return participantOrder;
-//        }
-//    }
-//}
+            // Loop through all rounds of the tournament
+            for (int round = 0; round < numberOfParticipants - 1; round++)
+            {
+                var games = new RoundGames();
+
+                // Pair participants for each round
+                for (int pairIndex = 0; pairIndex < numberOfParticipants / 2; pairIndex++)
+                {
+                    // Determine the indices of the paired participants
+                    int player1Index = participantOrder[pairIndex];
+                    int player2Index = participantOrder[numberOfParticipants - 1 - pairIndex];
+
+                    // Ensure the indices are valid
+                    if (player1Index < 0 || player2Index < 0 || player1Index >= numberOfParticipants || player2Index >= numberOfParticipants)
+                        throw new InvalidOperationException("Invalid participant index.");
+
+                    // Add the pair to the games list
+                    games.Add(player1Index + 1, player2Index + 1); // Adding 1 to convert from 0-based to 1-based index
+                }
+
+                // Add the games for this round to the tournament schedule
+                tournamentSchedule.Add(games);
+
+                // Rotate participant order for the next round
+                int lastParticipantIndex = participantOrder[numberOfParticipants - 2];
+                participantOrder.RemoveAt(numberOfParticipants - 2);
+                participantOrder.Insert(0, lastParticipantIndex);
+            }
+
+            return tournamentSchedule;
+        }
+
+        private List<int> RandomizeParticipantOrder(int numberOfParticipants)
+        {
+            var rnd = new Random();
+            var participantOrder = new List<int>();
+
+            // Generate a list of participant indices
+            for (int i = 0; i < numberOfParticipants; i++)
+            {
+                participantOrder.Add(i);
+            }
+
+            // Shuffle the list using Fisher-Yates algorithm
+            for (int i = numberOfParticipants - 1; i > 0; i--)
+            {
+                int swapIndex = rnd.Next(0, i + 1);
+                int temp = participantOrder[i];
+                participantOrder[i] = participantOrder[swapIndex];
+                participantOrder[swapIndex] = temp;
+            }
+
+            return participantOrder;
+        }
+    }
+}
