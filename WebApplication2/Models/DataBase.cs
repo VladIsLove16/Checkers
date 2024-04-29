98d4cf44aa605272b4b800475f0bd8bf1624d50b
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2024-04-30 00:13:35 +0300

    +DB

---
+++ WebApplication2/Models/DataBase.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication2.ViewModels;
+
+namespace WebApplication2.Models
+{
+    public static class DataBase
+    {
+        public static List<Tournament> TournamentList { get; set; } = new List<Tournament>();
+        public static SettingGame SettingGame { get; set; } = new SettingGame();
+        public static Judge Judge { get; internal set; }
+    }
+}
