536273546fc6647681f1039abf683934a4f0b7c5
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2024-04-30 00:13:26 +0300

    +Judge

---
+++ WebApplication2/Models/Judge.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication2.Models
+{
+    public class Judge
+    {
+        public string Name { get; set; } = "Не указано";
+        public string Education { get; set; } = "Не указано";
+    }
+}
