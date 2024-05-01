c3bb02754b3891e7c729751f8c3f3b4c341f3c2f
EVOALI1 <91939916+EVOALI1@users.noreply.github.com>
2024-05-01 21:37:54 +0300

    Update OlympicSortSystem.cs

--- WebApplication2/Models/SortSystems/OlympicSortSystem.cs
+++ WebApplication2/Models/SortSystems/OlympicSortSystem.cs
@@ -1,76 +1,51 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace WebApplication2.Models.SortSystems
 {
     public class OlympicSortSystem : SortSystem
     {
-        //        public List<RoundGames> CreateTournamentSchedule(int numberOfParticipants)
-        //        {
-        //            // Ensure the number of participants is valid
-        //            if (numberOfParticipants <= 0)
-        //                throw new ArgumentException("Number of participants must be greater than zero.");
+        private readonly List<Participant> participants;
 
-        //            var tournamentSchedule = new List<RoundGames>();
-        //            var participantOrder = RandomizeParticipantOrder(numberOfParticipants);
+        public OlympicSortSystem(List<Participant> participants)
+        {
+            this.participants = participants ?? throw new ArgumentNullException(nameof(participants));
+        }
 
-        //            // Loop through all rounds of the tournament
-        //            for (int round = 0; round < numberOfParticipants - 1; round++)
-        //            {
-        //                var games = new RoundGames();
-
-        //                // Pair participants for each round
-        //                for (int pairIndex = 0; pairIndex < numberOfParticipants / 2; pairIndex++)
-        //                {
-        //                    // Determine the indices of the paired participants
-        //                    int player1Index = participantOrder[pairIndex];
-        //                    int player2Index = participantOrder[numberOfParticipants - 1 - pairIndex];
-
-        //                    // Ensure the indices are valid
-        //                    if (player1Index < 0 || player2Index < 0 || player1Index >= numberOfParticipants || player2Index >= numberOfParticipants)
-        //                        throw new InvalidOperationException("Invalid participant index.");
-
-        //                    // Add the pair to the games list
-        //                    games.Add(player1Index + 1, player2Index + 1); // Adding 1 to convert from 0-based to 1-based index
-        //                }
-
-        //                // Add the games for this round to the tournament schedule
-        //                tournamentSchedule.Add(games);
-
-        //                // Rotate participant order for the next round
-        //                int lastParticipantIndex = participantOrder[numberOfParticipants - 2];
-        //                participantOrder.RemoveAt(numberOfParticipants - 2);
-        //                participantOrder.Insert(0, lastParticipantIndex);
-        //            }
-
-        //            return tournamentSchedule;
-        //        }
-
-        //        private List<int> RandomizeParticipantOrder(int numberOfParticipants)
-        //        {
-        //            var rnd = new Random();
-        //            var participantOrder = new List<int>();
-
-        //            // Generate a list of participant indices
-        //            for (int i = 0; i < numberOfParticipants; i++)
-        //            {
-        //                participantOrder.Add(i);
-        //            }
-
-        //            // Shuffle the list using Fisher-Yates algorithm
-        //            for (int i = numberOfParticipants - 1; i > 0; i--)
-        //            {
-        //                int swapIndex = rnd.Next(0, i + 1);
-        //                int temp = participantOrder[i];
-        //                participantOrder[i] = participantOrder[swapIndex];
-        //                participantOrder[swapIndex] = temp;
-        //            }
-
-        //            return participantOrder;
-        //        }
         public override List<RoundGames> CreateTournamentSchedule()
         {
-            throw new NotImplementedException();
+            if (participants.Count % 2 != 0)
+            {
+                throw new InvalidOperationException("The number of participants must be even for an Olympic-style tournament.");
+            }
+
+            var tournamentSchedule = new List<RoundGames>();
+            var shuffledParticipants = participants.OrderBy(p => Guid.NewGuid()).ToList();
+
+            int numRounds = shuffledParticipants.Count - 1;
+
+            for (int roundIndex = 0; roundIndex < numRounds; roundIndex++)
+            {
+                var games = new RoundGames();
+
+                for (int pairIndex = 0; pairIndex < shuffledParticipants.Count / 2; pairIndex++)
+                {
+                    var player1 = shuffledParticipants[pairIndex];
+                    var player2 = shuffledParticipants[shuffledParticipants.Count - 1 - pairIndex];
+
+                    games.Add(player1, player2);
+                }
+
+                tournamentSchedule.Add(games);
+
+                // Rotate participants for the next round (swap positions)
+                var lastParticipant = shuffledParticipants.Last();
+                shuffledParticipants.RemoveAt(shuffledParticipants.Count - 1);
+                shuffledParticipants.Insert(1, lastParticipant);
+            }
+
+            return tournamentSchedule;
         }
     }
 }
